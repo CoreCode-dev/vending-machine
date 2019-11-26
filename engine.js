eddbf6aa4b684bf010a66bd3bc1be222b52ba62a
Denis <denis372001@gmail.com>
2019-11-26 19:55:36 +0200

    START

---
+++ engine.js
@@ -0,0 +1,37 @@
+ let box = {};
+ 
+ //EXPEREMNTAL FUNCTIONS
+ function add_to_box(type, text, price){
+	 
+ }
+ 
+  function delete_type(text){
+	 
+	 
+ }
+ 
+ function check_size(){
+
+	let size_type_max = 7;
+	let size_typeBox_max = 5;
+	
+ }
+ 
+ //END EXPEREMNTAL FUNCTIONS
+ 
+ function set_box_default(){
+	 
+	 let default_labels = ["","","","","","",""];
+	 let default_colors = ["","","","","","",""];
+	 
+	 for(let i=0;i<7;i++){
+		 let bottle = { color:default_colors[i], label:default_labels[i]};
+	 }
+	 
+ }
+ 
+
+ 
+
+ 
+ 
