75d13c7a40984bca819afcf08591570f18b26736
Denis <denis372001@gmail.com>
2019-11-28 16:16:20 +0200

    VERSION5

--- engine.js
+++ engine.js
@@ -1,25 +1,46 @@
 let box =[];
 
 let Main = document.getElementsByClassName("Main");
+let Inventory = document.getElementsByClassName("inventory");
 let display = Main[0].children["display"];
 let display_ctx = display.getContext('2d');
 let hole = Main[0].children["hole"];
 let hole_ctx = hole.getContext('2d');
 
+let counter = Main[0].children["keyboard"].children["counter"];
+
+let money_slot = Inventory[0].children["pay"];
+
+let balance = 0;
+
+let card_flag = false;
+
 
 function setBox() { //120x50 px
-    let image = new Image();
-    let images = ["","","","","",""];
+    //let image = new Image();
+    let images = ["card","cash","card","cash","cash","cash"];
+
+
+
     let priceList = [2,6,4,3,2,3];
+
     image.src = "images/cash.png";
 
 
     let id = 0;
     for( let i =0; i<3;i++){
 
-        let section = {id : id, amount:3, pos:[10,10+50*i], image:image, price:priceList[id]};
+
+        let id_image = Math.floor(Math.random()*[1,2,3].length);
+        let image1 = new Image(); image1.src = "images/"+images[ id_image ]+".png";
+        images.splice(id_image, 1);
+        id_image = Math.floor(Math.random()*[1,2,3].length);
+        let image2 = new Image(); image2.src = "images/"+images[ id_image ]+".png";
+        images.splice(id_image, 1);
+
+        let section = {id : id, amount:3, pos:[10,10+50*i], image:image1, price:priceList[id]};
         id++;
-        let section2 = {id : id, amount:3, pos:[160,10+50*i], image:image, price:priceList[id]};
+        let section2 = {id : id, amount:3, pos:[160,10+50*i], image:image2, price:priceList[id]};
         id++;
         box.push(section);box.push(section2);
 
@@ -58,39 +79,61 @@ setInterval(CheckBox, 2000);
 
 
 
+function setBalance(){
 
-function getThing( id ){
-    hole_ctx.clearRect(0,0 , hole.width, hole.height);
-    printBox();
-    let section;
-    for(let i = 0; i<box.length;i++){
-        if(box[i].id==id){section = box[i];box[i].amount--;break;}
-    }
+    let data = money_slot.value;
+    money_slot.value="";
+    balance += (isFinite(data))?parseInt(data):0;
 
-    if(section!=undefined) {
-        let obj = {pos: section.pos.concat(), image: section.image};
+    counter.innerHTML=balance;
 
-        let timer = setInterval(function () {
-
-            printBox();
+}
 
 
-            if (obj.pos[1] === 190) {
+function getThing( id, cost){
+    if( card_flag||balance-cost>=0){
+        if(!card_flag)balance-=cost;
+        card_flag = false;
+        hole_ctx.clearRect(0, 0, hole.width, hole.height);
+        printBox();
+        let section;
+        for (let i = 0; i < box.length; i++) {
+            if (box[i].id == id) {
+                section = box[i];
+                box[i].amount--;
+                break;
+            }
+        }
+
+        if (section != undefined) {
+            let obj = {pos: section.pos.concat(), image: section.image};
+
+            let timer = setInterval(function () {
 
                 printBox();
-                hole_ctx.drawImage(obj.image,10,10,280,120);
-                clearInterval(timer);
-                return;
-            }
 
 
-            let x = obj.pos[0];
-            let y = obj.pos[1];
-            display_ctx.drawImage(obj.image, x, y, 120, 30);
-            console.log(1);
-            obj.pos[1]++;
+                if (obj.pos[1] === 190) {
 
-        }, 10);
+                    printBox();
+                    hole_ctx.drawImage(obj.image, 10, 10, 280, 120);
+                    clearInterval(timer);
+                    return;
+                }
+
+
+                let x = obj.pos[0];
+                let y = obj.pos[1];
+                display_ctx.drawImage(obj.image, x, y, 120, 30);
+                obj.pos[1]++;
+
+            }, 10);
+        }
+    }else{
+
+        card_flag = false;
+
     }
+    counter.innerHTML = balance;
 }
 
